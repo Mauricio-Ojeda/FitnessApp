f76c9facf7ec7cb0edbb59da7d559a3a35e1b589
Mauricio Ojeda <ojedam150@gmail.com>
2021-04-22 15:54:20 -0300

    apuntes componentes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import exerciseImg from '../images/exercise.png'
+import React from 'react'// siempre se importa react
+import exerciseImg from '../images/exercise.png' // exportamos la imagen para ser usada en el componente
 
+/* Los componentes permiten separar la interfaz de usuario en piezas independientes, 
+reutilizables y pensar en cada pieza de forma aislada.
+Aceptan entradas arbitrarias (llamadas “props”) y devuelven a React elementos que describen lo que debe aparecer en la pantalla.
+Se usan componentes para los elementos que se repiten. Existen componentes de clase (class) y funcionales (function).
+Se usa como buena practica crear los componenetes dentro de su propio archivo. para eso creamos la carpeta components y 
+dentro colocamos los componentes con la extension js */
+
+//componente de clase 
+/* se utiliza la palabra reservada class + Nombre de la clase y se hace una extencion (extends) de React.component
+ dentro de la clase si o si debemos definir una funcion que es  RENDER que va a retornar nuestro elemento del componente*/
 class Card extends React.Component {
     render(){
         return (
             <div>
                <div>
-                   <img src={exerciseImg}/>
+                   <img src={exerciseImg} alt="exercise"/>
                 </div> 
                 <div>
                     <h1>Technique Guides</h1>
@@ -17,4 +27,4 @@ class Card extends React.Component {
     }
 }
 
-export default Card
+export default Card //asi exportamos nuestro componente al index.js
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@
 // https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Card from './components/Card'
+import Card from './components/Card' //importamos nuestro componente Card
 
-const container = document.getElementById('root')
+const container = document.querySelector('#root');
 
 // ReactDOM.render(__QUE__, __DONDE__)
+// para renderear un componente usamos <Nombre de componente/>
 ReactDOM.render(<Card />, container)
