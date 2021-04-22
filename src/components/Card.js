c55b804a1e549c1bbc5ee75e63fbfac849c7f09c
Mauricio Ojeda <ojedam150@gmail.com>
2021-04-22 16:21:07 -0300

    apuntes css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import exerciseImg from '../images/exercise.png'
-import './styles/Card.css'
+import './styles/Card.css' // dentro de components creamos la carpeta styles y colocamos los estilos de los componentes.
 
 class Card extends React.Component {
-    
+    //colocamos las clases de css con el atributo className
     render(){
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card"> 
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exerciseImg} className="float-right"/>
+                            <img src={exerciseImg} className="float-right" alt="exercise"/>
                         </div> 
                         <div className="col-6 Fitness-Card-Info">
                             <h1>Technique Guides</h1>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'// instalamos bootstrap en las dependencias y lo importamos
 
 const container = document.getElementById('root')
 
